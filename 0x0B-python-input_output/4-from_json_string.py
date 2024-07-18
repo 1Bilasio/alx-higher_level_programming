b7930db8ed5774037f2d210eaf603c9516a59eb8
1Bilasio <sirbilasio@gmail.com>
2024-07-18 10:20:45 +0300

    Python Input/Output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" JSON-to-object function."""
+import json
+
+def from_json_string(my_str):
+    """function returns  Python object from a JSON string."""
+    return json.loads(my_str)
